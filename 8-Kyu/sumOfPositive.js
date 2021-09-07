1ea69e0e1f97a281cc023bf4a32a00d5c7bdae93
Cat Goncalves <catgoncalves.dev@gmail.com>
2021-09-07 15:01:15 -0400

    renamed code-wars 8-kyu folder

--- 8-Kyu/sumOfPositive.js
+++ 8-Kyu/sumOfPositive.js
@@ -4,3 +4,12 @@
 // Example [1,-4,7,12] => 1 + 7 + 12 = 20
 
 // Note: if there is nothing to sum, the sum is default to 0.
+
+function positiveSum(arr) {
+    return arr.filter(element => element >= 0).reduce((a, c) => a + c, 0)
+}
+
+// Another cool solution
+// function positiveSum(arr) {
+//   return arr.reduce((a,b)=> a + (b > 0 ? b : 0),0);
+// }
