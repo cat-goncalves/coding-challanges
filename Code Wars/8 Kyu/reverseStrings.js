a9d1c50d0ea5426807f2e6f15e53379267da137c
Catarina Goncalves <88456882+cat-goncalves@users.noreply.github.com>
2021-09-10 07:58:17 -0400

    Add files via upload

---
+++ Code Wars/8 Kyu/reverseStrings.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/5168bb5dfe9a00b126000018/solutions/javascript
+// Complete the solution so that it reverses the string passed into it.
+
+// 'world'  =>  'dlrow'
+
+function solution(str){
+  return str.split('').reverse().join('');  
+}
---
+++ Code Wars/8 Kyu/youCantCantCodeUnderPressure1.js
@@ -0,0 +1,2 @@
+// https://www.codewars.com/kata/53ee5429ba190077850011d4
+// Code as fast as you can! You need to double the integer and return it.
