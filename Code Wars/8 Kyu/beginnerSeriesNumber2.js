5c6763cb2aac899f582169426a5c5dead743345d
Cat Goncalves <catgoncalves.dev@gmail.com>
2021-09-11 20:09:02 -0400

    adding code wars

--- Code Wars/8 Kyu/beginnerSeriesNumber2.js
+++
@@ -1,9 +0,0 @@
-// https://www.codewars.com/kata/55f9bca8ecaa9eac7100004a
-function past(h, m, s){
-  //#Happy Coding! ^_^
-  let hour = h * 3600000;
-  let minute = m * 60000;
-  let second = s * 1000;
-  
-  return hour + minute + second
-} 
