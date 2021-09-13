d85bdfa04f0ced6b5095ec0fdbcddd81ebbe879e
Cat Goncalves <catgoncalves.dev@gmail.com>
2021-09-13 15:55:42 -0400

    calculateAverage

--- 8-Kyu/calculateAverage.js
+++ 8-Kyu/calculateAverage.js
@@ -1,1 +1,11 @@
 // https://www.codewars.com/kata/57a2013acf1fa5bfc4000921
+
+function find_average(array) {
+  if (array.length === 0){
+    return 0
+  } else {
+    return array.reduce((a, c) => a + c, 0) / array.length
+  }
+}
+
+//  When the string is empty, the average returns NaN, when it is expecting zero, so the the conditional logic here is checking that the string is not empty before performing the calculation
