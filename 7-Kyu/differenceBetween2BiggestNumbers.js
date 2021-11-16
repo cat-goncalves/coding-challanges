fa9f80518f716ef829e5522e5040a73e7946a118
Cat Goncalves <catgoncalves.dev@gmail.com>
2021-11-16 16:54:04 -0500

    Difference Between 2 Biggest Numbers, edits to Sum Of Array Of Singles

---
+++ 7-Kyu/differenceBetween2BiggestNumbers.js
@@ -0,0 +1,20 @@
+/*
+https://www.codewars.com/kata/55e3f27d5dee52d8dd0000a9
+
+You have an array of non-negative integers. You need to calculate the difference between the 1st biggest number and the 2nd biggest number of the array.
+
+    diffBig2([10, 5, 2]);
+In this case, the 1st biggest number is 10 and the 2nd biggest number is 5. So, the function returns 5, the result of 10 - 5.
+
+You can assume that the input array has 2 or more elements.
+
+The input array has the sort method disabled, so you will have to solve it in another way.
+*/
+
+function diffBig2(arr){
+  let highest = Math.max(...arr)
+  let highestIndex = arr.indexOf(highest)
+  arr.splice(highestIndex, 1)
+  let secondHighest = Math.max(...arr)
+  return highest - secondHighest
+}
