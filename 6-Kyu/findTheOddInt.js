20a86dd6a0f655790d653fd1cb75de176ff8d476
Cat Goncalves <catgoncalves.dev@gmail.com>
2021-11-17 00:53:40 -0500

    Find The Odd Int

---
+++ 6-Kyu/findTheOddInt.js
@@ -0,0 +1,31 @@
+/*
+https://www.codewars.com/kata/54da5a58ea159efa38000836/solutions/javascript
+
+Description:
+Given an array of integers, find the one that appears an odd number of times.
+
+There will always be only one integer that appears an odd number of times.
+
+Examples
+[7] should return 7, because it occurs 1 time (which is odd).
+[0] should return 0, because it occurs 1 time (which is odd).
+[1,1,2] should return 2, because it occurs 1 time (which is odd).
+[0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
+[1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
+*/
+
+function findOdd(A) {
+  let intMap = {}
+  for(let num of A){
+    if(intMap.hasOwnProperty(num)){
+      intMap[num]++
+    }else{
+      intMap[num] = 0
+    }
+  }
+  for(let prop in intMap){
+    if(intMap[prop] % 2 === 0){
+      return Number(prop)
+    }
+  }
+}
