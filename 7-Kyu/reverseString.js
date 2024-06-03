88a0ce6d251d175d6e2b5c41f40c98f353441adb
Cat Goncalves <catgoncalves.dev@gmail.com>
2024-06-03 09:12:44 -0400

    Reverse String

---
+++ 7-Kyu/reverseString.js
@@ -0,0 +1,16 @@
+/**
+ * Reverse String
+ * https://www.codewars.com/kata/5259b20d6021e9e14c0010d4/train/javascript
+ * 
+ * Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+
+Examples
+
+"This is an example!" ==> "sihT si na !elpmaxe"
+"double  spaces"      ==> "elbuod  secaps"s
+ * 
+ */
+
+function reverseWords(str) {
+  return str.split(' ').map((x) => x.split('').reverse().join('')).join(' ')
+}
